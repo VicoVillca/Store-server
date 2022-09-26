a38c57fffc8502846eac979f541f9ae96eeac1eb
VicoVillca <vicovillca@hotmail.com>
2022-09-26 13:28:57 -0400

    probamdo git

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("Lets Go!!");
+		System.out.println("Saludos desde Main!!");
 	}
 
 }
