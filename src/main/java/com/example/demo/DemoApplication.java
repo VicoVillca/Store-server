4671d693456880e7f2980bbaa117a01f06e77e33
VicoVillca <vicovillca@hotmail.com>
2022-09-26 13:37:31 -0400

    pusheamos otra rama

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,9 +8,9 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("--------------------");
-		System.out.println("Saludos desde Main!!");
-		System.out.println("--------------------");
+		System.out.println("*-----------------------*");
+		System.out.println("*  Saludos desde Main!! *");
+		System.out.println("*-----------------------*");
 	}
 
 }
