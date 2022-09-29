410a82633f418f9d0d5116d67af19a94f87c7ae1
VicoVillca <vicovillca@hotmail.com>
2022-09-29 00:49:06 -0400

    habilitamso cor para coneccion del forntend

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -11,6 +14,15 @@ public class DemoApplication {
 		System.out.println("*-------------------------*");
 		System.out.println("*  Saludos desde Heroku!! *");
 		System.out.println("*-------------------------*");
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+			}
+		};
 	}
 
 }
