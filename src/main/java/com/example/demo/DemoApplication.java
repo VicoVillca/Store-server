39a348176c2caf8505783a4069825e2e6f51e68a
VicoVillca <vicovillca@hotmail.com>
2022-10-02 09:51:13 -0400

    cambiamos el saludo de inicio

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("*-----------------------*");
-		System.out.println("*  Saludos desde Main!! *");
+		System.out.println("*  Saludos desde Main2!! *");
 		System.out.println("*-----------------------*");
 	}
 
